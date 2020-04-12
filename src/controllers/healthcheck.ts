36d503aaed5ee60287c901b6ed4698208e8a85b9
Yamboy1 <yamboyd1@gmail.com>
2020-04-13 03:36:15 +1200

    Update gitignore

--- src/controllers/healthcheck.ts
+++ src/controllers/healthcheck.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express'
 
 export const healthcheck = (req: Request, res: Response) => {
     res.status(200).send({
-        success: true,
         message: 'HealthCheck intouch-backend OK',
     });
 }
