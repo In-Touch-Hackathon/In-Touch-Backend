80207b6077af29dfe245fd6fe70f258c4b2f0de0
Yamboy1 <yamboyd1@gmail.com>
2020-04-13 03:37:36 +1200

    Move back to default firebase creds

--- src/libraries/firebase.ts
+++ src/libraries/firebase.ts
@@ -1,28 +1,8 @@
-import * as admin from 'firebase-admin'
-import { volunteer } from '../models/firebase'
+import * as firebase from 'firebase-admin'
 
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH)
-
-const db = admin.database()
-
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+firebase.initializeApp({
+    credential: firebase.credential.applicationDefault(),
     databaseURL: 'https://in-touch-hackathon.firebaseio.com'
 })
 
-const verify_firebase_token = async (idToken: string): Promise<string> => {
-    // idToken comes from the client app
-    let decodedToken = await admin.auth().verifyIdToken(idToken)
-    let uid = decodedToken.uid
-    return uid
-}
-
-const register_user = (user: volunteer) => {
-    let ref = db.ref("volunteer")
-    ref.set({
-
-    })
-}
-
-export {admin as firebase, verify_firebase_token, register_user}
+export { firebase }
