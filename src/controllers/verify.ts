69c4a51d4da9d69970f7bd036bed16570124c819
Yamboy1 <yamboyd1@gmail.com>
2020-04-13 04:12:46 +1200

    refactor twilio module

--- src/controllers/verify.ts
+++ src/controllers/verify.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express'
 
 export const verify = (req: Request, res: Response) => {
-    const { apiToken } = req.params
+    const {apiToken} = req.params
+
+    // let message = {
+    //     body: `Your In-Touch verification code is: ${code}`,
+    //     messagingServiceSid: process.env.TWILIO_SMS,
+    //     to: to //should be in the format of '+441632960675'
+    // }
+    // this.client.messages.create(message)
 }
--- src/libraries/twilio.ts
+++ src/libraries/twilio.ts
@@ -1,17 +1,5 @@
 import * as twilio from 'twilio'
 
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_SECRET;
+const client = twilio()
 
-const client = twilio(accountSid, authToken)
-
-const sms_verify = (to: string, code: string) => {
-    let message = {
-        body: `Your In-Touch verification code is: ${code}`,
-        messagingServiceSid: process.env.TWILIO_SMS,
-        to: to //should be in the format of '+441632960675'
-    }
-    this.client.messages.create(message)
-}
-
-export {client as twilio, sms_verify}
+export { client as twilio }
