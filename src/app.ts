4dc87e873e25b7b2fdeeedf1fc29fe0f5d08a386
Yamboy1 <yamboyd1@gmail.com>
2020-04-13 04:09:35 +1200

    fix dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import { Router, Request, Response } from 'express'
+import './dotenv'
+
+import { Router } from 'express'
 import * as express from 'express'
 import * as cors from 'cors'
 import * as helmet from 'helmet'
-import {config} from 'dotenv'
 import { routes } from './routes'
 
 
-// load .env
-config()
 
 const app = express()
 const router = Router()
